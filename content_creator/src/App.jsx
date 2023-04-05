99792a4b30fb1c9b40057de7cc64438a1c948cad
jashwanth0712 <jashwanth0712@gmail.com>
2023-04-05 23:55:31 +0530

    added subscriptions page

--- content_creator/src/App.jsx
+++ content_creator/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/navbar';
 import { GoogleLogin } from '@react-oauth/google';
 import PaymentForm from './components/paymentform';
 import Content_manager from './components/cms';
+import Subscriptionplans from './components/subscription_plans';
 
 function App() {
   
@@ -21,6 +22,7 @@ function App() {
           <Route path='/cms' element={<Content_manager />}></Route>
           <Route path='/payment' element={<PaymentForm />}></Route>
           <Route path='/form/:domain' element={<Forms />}></Route>
+          <Route path='/plans' element={<Subscriptionplans />}></Route>
           <Route path='/content-select' element={<SelectIndustry />} ></Route>
         </Routes>
       </Router>
--- content_creator/src/components/homepage.jsx
+++ content_creator/src/components/homepage.jsx
@@ -78,7 +78,7 @@ export default function HomePage() {
           autoplay
         />
       </div>
+      
     </div>
-
   );
 }
